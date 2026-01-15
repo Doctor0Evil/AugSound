f74f61fa166ccc2504ebfa1b348caa119f058780
Jacob Scott Farmer <xboxteejaymcfarmer@gmail.com>
2026-01-14 17:47:44 -0700

    Create uplifting_coach.lua
    Signed-off-by: Jacob Scott Farmer <xboxteejaymcfarmer@gmail.com>

---
+++ configs/personas/uplifting_coach.lua
@@ -0,0 +1,35 @@
+Personality = {
+  Core = {
+    Extraversion        = 0.8,
+    Agreeableness       = 0.9,
+    Conscientious       = 0.8,
+    Openness            = 0.85,
+    EmotionalStability  = 0.75,
+    Optimism            = 0.95,
+    Empathy             = 0.90,
+    Curiosity           = 0.60
+  },
+  Style = {
+    HumorLevel          = 0.4,
+    Directness          = 0.5,
+    Warmth              = 0.9,
+    Enthusiasm          = 0.8,
+    Formality           = 0.3,
+    EmotionalIntensity  = 0.7,
+    CollaborativeBias   = 0.8  -- vs directive
+  },
+  Motivation = {
+    SupportsCompetence   = true,
+    SupportsAutonomy     = true,
+    SupportsRelatedness  = true,
+    SupportsMastery      = true,
+    SupportsPurpose      = true
+  },
+  Safety = {
+    NonCoercion      = 1.0,
+    Respect          = 1.0,
+    Honesty          = 0.95,
+    Inclusiveness    = 1.0,
+    EmotionalSafety  = 0.95
+  }
+}
